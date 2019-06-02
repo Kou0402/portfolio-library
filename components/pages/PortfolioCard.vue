4623c6d51764b7b80d29741346be46b55cc8dbe9
kou0402 <kou.4869.biz@gmail.com>
2019-06-02 20:33:57 +0900

    Add PortfolioCard

---
+++ components/pages/PortfolioCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <a class="portfolio-card">
+    <div class="portfolio-card-image"></div>
+    <p class="portfolio-card-description"></p>
+  </a>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+.portfolio-card {
+  width: 18rem;
+  height: 27rem;
+  box-shadow: 3px 3px 10px 5px rgba(0, 0, 0, 0.1);
+  margin: 3rem;
+  padding: 0 1rem 0;
+  text-decoration: none;
+  .portfolio-card-image {
+    width: 90%;
+    height: 10rem;
+    background-color: aquamarine;
+    margin: 1rem auto;
+  }
+}
+
+@media screen and (max-width: 896px) {
+  .portfolio-card {
+    width: 100%;
+    height: 7rem;
+    box-shadow: initial;
+    margin: 0;
+    border-bottom: solid 3px $main-color;
+    padding: 0;
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    justify-content: space-between;
+    .portfolio-card-image {
+      width: 40%;
+      height: 90%;
+      margin: 0 0.3rem;
+    }
+    .portfolio-card-description {
+      margin: 0.3rem;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,38 @@
 <template>
-  <section></section>
+  <main>
+    <section class="contents-area">
+      <PortfolioCard></PortfolioCard>
+    </section>
+  </main>
 </template>
+
+<script>
+import PortfolioCard from '~/components/pages/PortfolioCard.vue'
+
+export default {
+  components: {
+    PortfolioCard
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+main {
+  .contents-area {
+    width: 80%;
+    margin: 3rem auto 0;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+}
+
+@media screen and (max-width: 896px) {
+  main {
+    .contents-area {
+      width: 100%;
+      margin: 0;
+    }
+  }
+}
+</style>
