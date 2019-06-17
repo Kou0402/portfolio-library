725a208282ca98cdcd0b6338cb843a1a2e798d9a
kou0402 <kou.4869.biz@gmail.com>
2019-06-17 22:42:53 +0900

    Test firestore end

--- pages/post.vue
+++ pages/post.vue
@@ -6,34 +6,6 @@
   </main>
 </template>
 
-<script>
-import firebase from '~/plugins/firebase.js'
-
-const db = firebase.firestore()
-
-// データの追加
-const addItem = content => {
-  return db
-    .collection('test')
-    .add({
-      ...content,
-      createdAt: new Date() // createAt フィールドがないとエラーが表示されるっぽい。
-    })
-    .then(res => {
-      console.log('Add Document with ID:', res.id)
-      return res.id
-    })
-    .catch(err => {
-      console.error('Error: Add Document', err)
-      throw err
-    })
-}
-
-export default {
-  mounted() {
-    addItem()
-  }
-}
-</script>
+<script></script>
 
 <style lang="scss" scoped></style>
