26ded8b20cf03e8322e966acd2fbe794e47191fc
kou0402 <kou.4869.biz@gmail.com>
2019-09-08 19:21:55 +0900

    Add menu.vue

---
+++ components/layouts/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <nuxt-link to="/">
+      TOP
+    </nuxt-link>
+    <nuxt-link to="/">
+      NEW
+    </nuxt-link>
+    <nuxt-link to="login">
+      POST
+    </nuxt-link>
+  </nav>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Hero></Hero>
+    <Menu></Menu>
     <nuxt />
   </div>
 </template>
 
 <script>
 import Hero from '~/components/layouts/Hero.vue'
+import Menu from '~/components/layouts/Menu.vue'
 
 export default {
   components: {
-    Hero
+    Hero,
+    Menu
   }
 }
 </script>
