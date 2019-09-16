3a7b755a87aae5de23c7f2ac3b1779664c996ab7
kou0402 <kou.4869.biz@gmail.com>
2019-09-16 23:35:02 +0900

    Delete test log

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,26 +1,18 @@
 import firebase from '@/plugins/firebase'
 
 export default async ({ req, route, redirect }) => {
-  console.log(route.path)
   if (!['/login'].includes(route.path)) {
-    console.log('1')
     return
   }
-  console.log('2')
   let isLogin = false
-  const res = await firebase.auth().onAuthStateChanged(user => {
-    console.log('3')
+  await firebase.auth().onAuthStateChanged(user => {
     if (user) {
       isLogin = true
     } else {
       isLogin = false
     }
   })
-  console.log(res)
-  console.log('4')
-  console.log(isLogin)
   if (isLogin) {
-    console.log('post')
     return redirect('/post')
   }
 }
