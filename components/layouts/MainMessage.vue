b73fe123345cd3cfe10db8eba367c5d8580efcf8
kou0402 <kou.4869.biz@gmail.com>
2019-09-22 18:57:26 +0900

    Add MainMessage.vue

---
+++ components/layouts/MainMessage.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="main-message">
+    <h2 class="message">
+      あなたのポートフォリオを共有しよう<br />ポートフォリオ共有サイト『Portfolio Library』
+    </h2>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.main-message {
+  background-color: $base-color;
+  height: 22vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .message {
+    color: $main-color2;
+    text-align: center;
+    font-size: 2.2rem;
+    letter-spacing: 0.5vw;
+    line-height: 6.5vh;
+    font-weight: 500;
+  }
+}
+</style>
