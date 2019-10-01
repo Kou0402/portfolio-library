4b3ff1d798635bafad500de80f3950bc9f257953
kou0402 <kou.4869.biz@gmail.com>
2019-10-01 19:55:48 +0900

    update portfolio style

--- components/pages/PortfolioCard.vue
+++ components/pages/PortfolioCard.vue
@@ -26,13 +26,13 @@ export default {
 
 <style lang="scss" scoped>
 .portfolio-card {
-  width: 340px;
-  height: 240px;
+  width: 300px;
+  height: 260px;
   box-shadow: 3px 3px 10px 5px rgba(0, 0, 0, 0.1);
   text-decoration: none;
   .portfolio-capture {
-    width: 100%;
-    height: 75%;
+    width: 300px;
+    height: 200px;
   }
   .portfolio-title {
     color: $accent-color1;
